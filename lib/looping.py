bddbea93205a59fcc50619a0f939516028df292a
Abdisemed Abdullahi <abdisemed20@gmail.com>
2023-11-21 17:57:38 +0300

    completed

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    
+    i = 10
+    while i > 0:
+        print(f"{i}")
+        i -= 1
+    print("Happy New Year!") 
+
     pass
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    
+    squared_list = [element * element for element in int_list]
+
+    return squared_list
+    
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+
+
